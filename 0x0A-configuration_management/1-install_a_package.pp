d407c15c524bce4c695f417e675d913ca48eec8b
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-11-24 22:46:48 +0300

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# install Flask version 2.1.0 using pip3
+# install puppet-lint -v 2.1.0
 
-package { 'Flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
