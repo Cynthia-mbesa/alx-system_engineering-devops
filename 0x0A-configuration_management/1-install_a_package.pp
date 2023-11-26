33bab1550a4a374a432f13a7319082930a4d9920
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-11-26 15:19:12 +0300

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
 # Install flask (2.1.0)
-package {'flask':
+package {'Flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+exec { 'install_werkzeug':
+command => '/usr/bin/pip3 install -q --upgrade werkzeug==2.1.1',
 }
