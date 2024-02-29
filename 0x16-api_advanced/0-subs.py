bdebcb3c29bbf7e82cbdc2c0dda242f8f51192f8
Cynthia-mbesa <cynthiambesa@gmail.com>
2024-02-29 23:43:28 +0300

    correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-""" Function that queries the Reddit API """
-import requests
-import sys
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """  Args:
-        subreddit: subreddit name
-    Returns:
-        number of subscribers to the subreddit,
-        or 0 if subreddit requested is invalid"""
-    headers = {'User-Agent': 'My-User-Agent'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    if response.status_code == 200:
-        return (response.json().get("data").get("subscribers"))
-    return (0)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code != 200:
+        return 0
+
+    try:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    except KeyError:
+        return 0
+
+# Test cases
+print(number_of_subscribers("existing_subreddit"))  # Output: OK
+print(number_of_subscribers("nonexisting_subreddit"))  # Output: OK
