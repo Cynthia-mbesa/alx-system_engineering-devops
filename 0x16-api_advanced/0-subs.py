5da1504af0c95aba995bdd9ada209611a275c909
Cynthia-mbesa <cynthiambesa@gmail.com>
2024-02-29 23:48:46 +0300

    correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,14 @@
 """Module for task 0"""
 
 
-def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+import requests
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "My-User-Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code != 200:
         return 0
 
@@ -18,9 +17,14 @@ def number_of_subscribers(subreddit):
         data = response.json()
         subscribers = data["data"]["subscribers"]
         return subscribers
-    except KeyError:
+    except (KeyError, ValueError):
         return 0
 
-# Test cases
-print(number_of_subscribers("existing_subreddit"))  # Output: OK
-print(number_of_subscribers("nonexisting_subreddit"))  # Output: OK
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        print(number_of_subscribers(subreddit))
